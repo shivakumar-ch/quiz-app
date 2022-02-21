9961c51d7b813c43c91adb44acc04b3308e05067
Ashok Ammineni <ashok.ammineni@CaprusIT.local>
2022-02-21 17:12:53 +0530

    Home Route

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,26 @@
 import { Container, Box } from "@mui/material";
 
 function Home() {
+  const loginnnn = async () => {
+    const posts = await fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name1: "ashok",
+      }),
+    });
+    const a = await posts.json();
+    console.log(a);
+  };
+
   return (
     <div>
       <Container>
         <Box>
           <h1>hai</h1>
+          <button onClick={loginnnn}>Login</button>
         </Box>
       </Container>
     </div>
